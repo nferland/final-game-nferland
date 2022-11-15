f9ee24b3eb1adf73387c31e6937461a1a2a620c7
Nicholas Ferland <nicholas.ferland03@gmail.com>
2022-11-15 13:30:38 -0500

    feat: add fps counter

--- src/cegepst/finalGame/Hud.java
+++ src/cegepst/finalGame/Hud.java
@@ -1,5 +1,6 @@
 package cegepst.finalGame;
 
+import cegepst.engine.GameTime;
 import cegepst.engine.graphics.Buffer;
 
 import java.awt.*;
@@ -7,6 +8,7 @@ import java.awt.*;
 public class Hud {
 
     public void draw(Buffer buffer) {
+        buffer.drawString("FPS: " + GameTime.getCurrentFps(), 10, 20, Color.WHITE);
         buffer.drawString("TOGGLE FULLSCREEN: <T>, FIRE SOUND: <SPACE>, TOGGLE MUSIC: <M>, QUIT: <Q>", 10, 584, Color.WHITE);
     }
 }
