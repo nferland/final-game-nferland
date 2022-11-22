64c7e6eb8b245be8f37ee1997b187dab35968462
Nicholas Ferland <nicholas.ferland03@gmail.com>
2022-11-22 09:50:58 -0500

    feat: add calcul for distance between two entity
    The Pythagorean Theorem was finally useful outside of maths.

--- src/cegepst/engine/EngineMath.java
+++ src/cegepst/engine/EngineMath.java
@@ -1,5 +1,7 @@
 package cegepst.engine;
 
+import cegepst.engine.entities.StaticEntity;
+
 public class EngineMath {
 
     public static int halfOf(int value) {
@@ -8,5 +10,15 @@ public class EngineMath {
 
     public static int threeQuarterOf(int value) {
         return (3 * value) / 4;
+    }
+
+    public static int squareOf(int value) {
+        return value * value;
+    }
+
+    public static int getDistanceBetween(StaticEntity entity1, StaticEntity entity2) {
+        int deltaX = Math.abs(entity1.getX() - entity2.getX());
+        int deltaY = Math.abs(entity1.getY() - entity2.getY());
+        return (int) Math.floor(Math.sqrt(squareOf(deltaX) + squareOf(deltaY)));
     }
 }
