fade7668bbd15048b1688ba3e81286a621e7c74c
Nicholas Ferland <nicholas.ferland03@gmail.com>
2022-11-22 10:40:13 -0500

    feat: health bar color change

--- src/cegepst/finalGame/Hud.java
+++ src/cegepst/finalGame/Hud.java
@@ -9,14 +9,24 @@ import java.awt.*;
 public class Hud {
 
     public void draw(Buffer buffer, Player player) {
-        buffer.drawString("FPS: " + GameTime.getCurrentFps(), 10, 20, Color.WHITE);
+        buffer.drawString("FPS: " + GameTime.getCurrentFps(), 10, 15, Color.WHITE);
         buffer.drawString("TOGGLE FULLSCREEN: <T>, FIRE SOUND: <SPACE>, TOGGLE MUSIC: <M>, QUIT: <Q>", 10, 584, Color.WHITE);
         drawPlayerHealthBar(buffer, player);
     }
 
     private void drawPlayerHealthBar(Buffer buffer,Player player) {
         double healthBarWidth = player.getHealthPoint() / (double) player.getMaxHealthPoint();
-        buffer.drawRectangle(15, 20, 210, 35, new Color(0, 0, 0));
-        buffer.drawRectangle(20, 25, (int) (200 * healthBarWidth), 25, new Color(255, 0, 0));
+        buffer.drawRectangle(15, 20, 210, 35, Color.BLACK);
+        buffer.drawRectangle(20, 25, (int) (200 * healthBarWidth), 25, getHealthBarColor(healthBarWidth));
+    }
+
+    private Color getHealthBarColor(double width) {
+        if (width >= 0.6) {
+            return Color.GREEN;
+        }
+        if(width >= 0.3) {
+            return Color.yellow;
+        }
+        return Color.red;
     }
 }
