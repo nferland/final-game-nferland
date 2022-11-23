afed67f11cd8ddce38063fc91e60cbba91b13e5f
Nicholas Ferland <nicholas.ferland03@gmail.com>
2022-11-23 09:04:58 -0500

    refactor: moved health management into MovableEntity

--- src/cegepst/engine/entities/ControllableEntity.java
+++ src/cegepst/engine/entities/ControllableEntity.java
@@ -2,12 +2,11 @@ package cegepst.engine.entities;
 
 import cegepst.engine.controls.Direction;
 import cegepst.engine.controls.MovementController;
+import cegepst.engine.entities.stateMachines.AttackState;
 
 public abstract class ControllableEntity extends MovableEntity {
 
     private final MovementController controller;
-    private int maxHealthPoint = 10;
-    private int healthPoint = maxHealthPoint;
 
     public ControllableEntity(MovementController controller) {
         this.controller = controller;
@@ -21,48 +20,6 @@ public abstract class ControllableEntity extends MovableEntity {
         Direction direction = controller.getMoveDirection();
         if (direction != null) {
             move(direction);
-        }
-    }
-
-    public void hurt(int damage) {
-        healthPoint -= damage;
-        if (healthPoint <= 0) {
-            healthPoint = 0;
-            die();
-        }
-    }
-
-    public void hurt(int damage, Direction kbDirection) {
-        hurt(damage);
-        knockBack(kbDirection);
-    }
-
-    public int getMaxHealthPoint() {
-        return maxHealthPoint;
-    }
-
-    public void setMaxHealthPoint(int maxHealthPoint) {
-        this.maxHealthPoint = maxHealthPoint;
-    }
-
-    public int getHealthPoint() {
-        return healthPoint;
-    }
-
-    public void setHealthPoint(int healthPoint) {
-        this.healthPoint = healthPoint;
-    }
-
-    private void die() {
-        // TODO: kill the player and restart game
-    }
-
-    private void knockBack(Direction direction) {
-        switch (direction){
-            case UP -> teleport(getX(), getY() - 8);
-            case LEFT -> teleport(getX() - 8, getY());
-            case DOWN -> teleport(getX(), getY() + 8);
-            case RIGHT -> teleport(getX() + 8, getY());
         }
     }
 }
